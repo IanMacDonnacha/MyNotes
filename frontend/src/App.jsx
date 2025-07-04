28c51242297f1d96784a17ba1bbd59f06a033279
ianmacdonnacha <91433149+IanMacDonnacha@users.noreply.github.com>
2025-07-04 21:17:10 +0100

    NavBar created for thinkboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,10 @@ import { Routes, Route } from 'react-router';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';
-import toast from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success('Nice')} className="btn btn-primary">
-        Click Here
-      </button>
+    <div data-theme="forest">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-screen">
+      <NavBar />
+    </div>
+  );
 };
 
 export default HomePage;
